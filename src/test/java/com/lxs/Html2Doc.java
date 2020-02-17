9cb12c0b5e9fe84d96361402f8a3e77c59d1e128
liuxiaosu <liuxiaosu@hyperchain.cn>
2020-02-17 17:31:49 +0800

    html2doc

--- src/test/java/com/lxs/Html2Doc.java
+++ src/test/java/com/lxs/Html2Doc.java
@@ -1,17 +1,8 @@
 package com.lxs;
-import java.io.*;
-import java.net.*;
+import cn.edu.hfut.dmic.htmlbot.contentextractor.ContentExtractor;
 
 public class Html2Doc {
     public static void main(String[] args) throws Exception {
-        // TODO Auto-generated method stub
-        URL oracle = new URL("https://docs.oracle.com/javase/tutorial/networking/urls/readingURL.html");
-        BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
-
-        String inputLine;
-        while((inputLine = in.readLine()) != null) {
-            System.out.println(inputLine);
-        }
-        in.close();
+        String content= ContentExtractor.getContentByURL("");
     }
 }
